dc395e7270cca23786deee503f64f6787a56ca4f
Gianarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-01-07 16:22:15 -0300

    Adjusting packages

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 
-require('./video_maker/index')().catch(console.log);
+let win;
+
+function createWindow() {
+
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        show: false
+    });
+
+    win.on('ready-to-show', () => { win.show() });
+
+    win.loadFile('app/pages/main.html');
+
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+})
