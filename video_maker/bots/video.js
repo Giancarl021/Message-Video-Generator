02fa486d174820d10b3a17692ee7a0801abfb7e2
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-01-19 15:33:34 -0300

    Error in bots/video.js, need fix

--- video_maker/bots/video.js
+++ video_maker/bots/video.js
@@ -2,8 +2,8 @@ const printer = require('./print');
 const {buildPath, buildUnpacked} = require('./path');
 const videoshow = require('videoshow');
 const ffmpeg = require('fluent-ffmpeg');
-const ffmpegPath = buildUnpacked(require('@ffmpeg-installer/ffmpeg').path);
-const ffprobePath = buildUnpacked(require('@ffprobe-installer/ffprobe').path);
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path; // ERROR HERE
+const ffprobePath = require('@ffprobe-installer/ffprobe').path; // ERROR HERE
 const videoOptions = require('./../data/config').video;
 ffmpeg.setFfmpegPath(ffmpegPath);
 ffmpeg.setFfprobePath(ffprobePath);
